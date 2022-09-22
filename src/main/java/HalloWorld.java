9bf1a4c43a254521db4682a893777e8d64b51dbb
moaiash <st179737@stud.uni-stuttgart.de>
2022-09-22 14:23:34 +0200

    Initial commit

---
+++ src/main/java/HalloWorld.java
@@ -0,0 +1,17 @@
+import hallo.auto;
+
+public class HalloWorld<a> {
+    HalloWorld<Integer> halloWorld = new HalloWorld<Integer>();
+    public static void main(String[] args) {
+        System.out.println("I love pizza");
+        System.out.println("It`s so yummy");
+        System.out.println("It`s so yummy");
+    auto auto = new auto("keyboard");
+        System.out.println(auto.getName());
+
+    }
+
+}
+
+
+
---
+++ src/main/java/hallo/auto.java
@@ -0,0 +1,18 @@
+package hallo;
+
+public class auto {
+    private String name;
+    public auto(String name){
+        this.name = name ;
+    }
+    public String getName(){
+        return name;
+    }
+    public static void main(String[] args) {
+        System.out.println("hallo");
+        System.out.println("hallo");
+        System.out.println("hallo");
+
+
+    }
+}
